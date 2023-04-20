3d3792ac9dc375bbda602d09a02626b7046b7204
erfelipe <erfelipe@yahoo.com.br>
2023-04-20 18:58:28 -0300

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("teste do js");
+
+function calculaIMC() {
+    let peso, altura, resp, classificacao;
+    peso = parseFloat( document.getElementById("edtPeso").value ) ; 
+    altura = parseFloat( document.getElementById("edtAltura").value ) ;
+    
+    resp = peso / (altura * altura) ;
+
+    if (resp <= 18.5) {
+        classificacao = " abaixo do peso";
+    } else if (resp <= 24.9) {
+        classificacao = " peso ideal"; 
+    } else if (resp <= 29.9) {
+        classificacao = " levemente acima do peso";
+    } else {
+        classificacao = " programador, termine a tabela";
+    }
+
+    document.getElementById("resp").innerText = "O seu IMC é: " + resp + " a sua classificação é a seguinte:" + classificacao;
+
+}
